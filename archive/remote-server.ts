81acfe496530439d470e21fca3e16ff2fb749202
krypek <krypek@tuta.io>
2025-04-03 15:21:30 +0200

    Remove ./archive

--- archive/remote-server.ts
+++
@@ -1,9 +0,0 @@
-import { Server, ServerSettings } from './local-server'
-
-export interface RemoteServerSettings extends ServerSettings {
-    slotName: string
-    host: string
-    port: number
-}
-
-export class RemoteServer implements Server<RemoteServerSettings> {}
