5fab01a467f0ff62a86f56adfb5011bbf8b57fe8
krypek <krypek@tuta.io>
2025-06-26 12:38:29 +0200

    Bump ccmodmanager and nax-ccuilib required versions in ccmod.json

--- src/plugin.ts
+++ src/plugin.ts
@@ -42,9 +42,6 @@ export default class Multibakery implements PluginClass {
     }
 }
 
-/* TODO: bump nax-ccuilib version */
-/* TODO: bump ccmodmanager version */
-
 poststart(() => {
     if (window.crossnode?.options.test) return
 
