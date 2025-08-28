40c8beda6532be84f3630d536a5e8d22b2808295
krypek <krypek@tuta.io>
2025-08-28 21:07:44 +0200

    Move dummy.DummyPlayer state code to ig_ENTITY_Player-base.ts

--- src/state/entity/dummy_DummyPlayer.ts
+++ src/state/entity/dummy_DummyPlayer.ts
@@ -4,35 +4,18 @@ import { prestart } from '../../plugin'
 import { RemoteServer } from '../../server/remote/remote-server'
 import { StateKey } from '../states'
 import { shouldCollectStateData, StateMemory } from '../state-util'
-import * as igEntityCombatant from './ig_ENTITY_Combatant-base'
-import { notifyMapAndPlayerInsts } from '../../server/ccmap/ccmap'
+import * as igEntityPlayer from './ig_ENTITY_Player-base'
 
 declare global {
     namespace dummy {
-        interface DummyPlayer extends StateMemory.MapHolder<StateKey> {
+        interface DummyPlayer {
             createNetid(this: this, x: number, y: number, z: number, settings: dummy.DummyPlayer.Settings): string
         }
     }
 }
-function getSkills(this: dummy.DummyPlayer): number[] {
-    const skillCount = this.model.skills.length
-    const skills = new Array(skillCount)
-    for (let i = 0; i < skillCount; i++) {
-        skills[i] = this.model.skills[i] ? 1 : 0
-    }
-    return skills
-}
-function setSkills(this: dummy.DummyPlayer, skills: Record<number, number>) {
-    for (const idStr in skills) {
-        const id = parseInt(idStr)
-        this.model.skills[id] = skills[id] ? sc.skilltree.skills[id] : null
-    }
-}
 
-export type DummyState = ReturnType<typeof getState>
+type Return = ReturnType<typeof getState>
 function getState(this: dummy.DummyPlayer, player?: StateKey) {
-    const chargeLevel = this.charging.time == -1 ? 0 : this.getCurrentChargeLevel() || 1
-
     const memory = StateMemory.getBy(this, player)
     return {
         isControlBlocked: memory.diff(this.data.isControlBlocked),
@@ -40,37 +23,21 @@ function getState(this: dummy.DummyPlayer, player?: StateKey) {
         currentMenu: memory.diff(this.data.currentMenu),
         currentSubState: memory.diff(this.data.currentSubState),
 
-        ...igEntityCombatant.getState.call(this, memory),
-
-        interactObject: memory.diff(this.interactObject?.entity?.netid),
-
-        head: memory.diff(this.model.equip.head),
-        leftArm: memory.diff(this.model.equip.leftArm),
-        rightArm: memory.diff(this.model.equip.rightArm),
-        torso: memory.diff(this.model.equip.torso),
-        feet: memory.diff(this.model.equip.feet),
-
-        level: memory.diff(this.model.level),
-        items: !player || this == player.dummy ? memory.diffRecord(this.model.items) : undefined,
-        skillPoints: !player || this == player.dummy ? memory.diffRecord(this.model.skillPoints) : undefined,
-        skills: !player || this == player.dummy ? memory.diffRecord(getSkills.call(this)) : undefined,
-
-        charge: memory.diff(chargeLevel),
-        element: memory.diff(this.model.currentElementMode),
+        ...igEntityPlayer.getState.call(this, player, memory),
     }
 }
 
-function setState(this: dummy.DummyPlayer, state: DummyState) {
+function setState(this: dummy.DummyPlayer, state: Return) {
     if (state.isControlBlocked !== undefined) this.data.isControlBlocked = state.isControlBlocked
     if (state.inCutscene !== undefined) this.data.inCutscene = state.inCutscene
     if (state.currentMenu !== undefined) this.data.currentMenu = state.currentMenu
     if (state.currentSubState !== undefined) this.data.currentSubState = state.currentSubState
 
     if (state.currentAnim !== undefined && this.currentAnim != state.currentAnim) {
-        const anim = state.currentAnim
-
         if (
-            (anim == 'attack' || anim == 'attackRev' || anim == 'attackFinisher') &&
+            (state.currentAnim == 'attack' ||
+                state.currentAnim == 'attackRev' ||
+                state.currentAnim == 'attackFinisher') &&
             this.inputManager.inputType == ig.INPUT_DEVICES.KEYBOARD_AND_MOUSE &&
             this.model.getCore(sc.PLAYER_CORE.THROWING) &&
             sc.options.get('close-circle')
@@ -79,81 +46,7 @@ function setState(this: dummy.DummyPlayer, state: DummyState) {
         }
     }
 
-    igEntityCombatant.setState.call(this, state)
-
-    if (state.spLevel !== undefined) {
-        this.model.spLevel = state.spLevel
-    }
-
-    /* footstep sounds */
-    function getSoundFromColl(coll: ig.CollEntry, type: keyof typeof sc.ACTOR_SOUND): sc.ACTOR_SOUND_BASE {
-        var c = ig.terrain.getTerrain(coll, true, true),
-            e = sc.ACTOR_SOUND[type] || sc.ACTOR_SOUND.none
-        return (e as any)[c] ?? e[ig.TERRAIN_DEFAULT]
-    }
-    if (
-        !this.jumping &&
-        !this.animationFixed &&
-        this.stepFx.frames &&
-        !Vec2.isZero(this.coll.accelDir) &&
-        this.coll.relativeVel >= ig.ACTOR_RUN_THRESHOLD
-    ) {
-        const frame = this.animState.getFrame()
-        if (frame != this.stepFx.lastFrame) {
-            const sound = getSoundFromColl(this.coll, this.soundType)
-            if (frame == this.stepFx.frames[0]) {
-                ig.SoundHelper.playAtEntity(sound.step1!, this, null, null, 700)
-            } else if (frame == this.stepFx.frames[1]) {
-                ig.SoundHelper.playAtEntity(sound.step2!, this, null, null, 700)
-            }
-            this.stepFx.lastFrame = frame
-        }
-    } else this.stepFx.lastFrame = -1
-
-    if (state.interactObject) {
-        const entity = ig.game.entitiesByNetid[state.interactObject]
-        assert('pushPullable' in entity && entity.pushPullable instanceof sc.PushPullable)
-        this.interactObject = entity.pushPullable
-    } else this.interactObject = null
-
-    let updateStats = false
-    // prettier-ignore
-    {
-        if (state.head !== undefined) { updateStats = true; this.model.equip.head = state.head }
-        if (state.leftArm !== undefined) { updateStats = true; this.model.equip.leftArm = state.leftArm }
-        if (state.rightArm !== undefined) { updateStats = true; this.model.equip.rightArm = state.rightArm }
-        if (state.torso !== undefined) { updateStats = true; this.model.equip.torso = state.torso }
-        if (state.feet !== undefined) { updateStats = true; this.model.equip.feet = state.feet }
-    }
-
-    if (state.level !== undefined) {
-        this.model.level = state.level
-        /* I don't want to even start thinking about making this unique for every player */
-        sc.inventory.updateScaledEquipment(state.level)
-        notifyMapAndPlayerInsts(this.model, sc.PLAYER_MSG.LEVEL_CHANGE, null)
-    }
-    if (state.items) StateMemory.applyChangeRecord(this.model.items, state.items)
-    if (state.skillPoints) StateMemory.applyChangeRecord(this.model.skillPoints, state.skillPoints)
-    if (state.skills) {
-        setSkills.call(this, state.skills)
-        updateStats = true
-    }
-
-    if (updateStats) {
-        this.model.updateStats()
-    }
-
-    if (state.charge !== undefined) {
-        if (state.charge == 0) {
-            this.charging.time = 1
-            this.clearCharge()
-        } else {
-            this.showChargeEffect(state.charge)
-        }
-    }
-    if (state.element !== undefined) {
-        this.model.currentElementMode = state.element
-    }
+    igEntityPlayer.setState.call(this, state)
 }
 
 prestart(() => {
---
+++ src/state/entity/ig_ENTITY_Player-base.ts
@@ -0,0 +1,136 @@
+import { assert } from '../../misc/assert'
+import { prestart } from '../../plugin'
+import { notifyMapAndPlayerInsts } from '../../server/ccmap/ccmap'
+import { StateMemory } from '../state-util'
+import { StateKey } from '../states'
+import * as igEntityCombatant from './ig_ENTITY_Combatant-base'
+
+declare global {
+    namespace ig.ENTITY {
+        interface Player extends StateMemory.MapHolder<StateKey> {}
+    }
+}
+
+function getSkills(this: ig.ENTITY.Player): number[] {
+    const skillCount = this.model.skills.length
+    const skills = new Array(skillCount)
+    for (let i = 0; i < skillCount; i++) {
+        skills[i] = this.model.skills[i] ? 1 : 0
+    }
+    return skills
+}
+
+function setSkills(this: ig.ENTITY.Player, skills: Record<number, number>) {
+    for (const idStr in skills) {
+        const id = parseInt(idStr)
+        this.model.skills[id] = skills[id] ? sc.skilltree.skills[id] : null
+    }
+}
+
+type Return = ReturnType<typeof getState>
+export function getState(this: ig.ENTITY.Player, player?: StateKey, memory?: StateMemory) {
+    const chargeLevel = this.charging.time == -1 ? 0 : this.getCurrentChargeLevel() || 1
+
+    memory ??= StateMemory.getBy(this, player)
+    return {
+        ...igEntityCombatant.getState.call(this, memory),
+
+        interactObject: memory.diff(this.interactObject?.entity?.netid),
+
+        head: memory.diff(this.model.equip.head),
+        leftArm: memory.diff(this.model.equip.leftArm),
+        rightArm: memory.diff(this.model.equip.rightArm),
+        torso: memory.diff(this.model.equip.torso),
+        feet: memory.diff(this.model.equip.feet),
+
+        level: memory.diff(this.model.level),
+        items: !player || this == player.dummy ? memory.diffRecord(this.model.items) : undefined,
+        skillPoints: !player || this == player.dummy ? memory.diffRecord(this.model.skillPoints) : undefined,
+        skills: !player || this == player.dummy ? memory.diffRecord(getSkills.call(this)) : undefined,
+
+        charge: memory.diff(chargeLevel),
+        element: memory.diff(this.model.currentElementMode),
+    }
+}
+
+export function setState(this: ig.ENTITY.Player, state: Return) {
+    igEntityCombatant.setState.call(this, state)
+
+    if (state.spLevel !== undefined) {
+        this.model.spLevel = state.spLevel
+    }
+
+    /* footstep sounds */
+    function getSoundFromColl(coll: ig.CollEntry, type: keyof typeof sc.ACTOR_SOUND): sc.ACTOR_SOUND_BASE {
+        var c = ig.terrain.getTerrain(coll, true, true),
+            e = sc.ACTOR_SOUND[type] || sc.ACTOR_SOUND.none
+        return (e as any)[c] ?? e[ig.TERRAIN_DEFAULT]
+    }
+    if (
+        !this.jumping &&
+        !this.animationFixed &&
+        this.stepFx.frames &&
+        !Vec2.isZero(this.coll.accelDir) &&
+        this.coll.relativeVel >= ig.ACTOR_RUN_THRESHOLD
+    ) {
+        const frame = this.animState.getFrame()
+        if (frame != this.stepFx.lastFrame) {
+            const sound = getSoundFromColl(this.coll, this.soundType)
+            if (frame == this.stepFx.frames[0]) {
+                ig.SoundHelper.playAtEntity(sound.step1!, this, null, null, 700)
+            } else if (frame == this.stepFx.frames[1]) {
+                ig.SoundHelper.playAtEntity(sound.step2!, this, null, null, 700)
+            }
+            this.stepFx.lastFrame = frame
+        }
+    } else this.stepFx.lastFrame = -1
+
+    if (state.interactObject) {
+        const entity = ig.game.entitiesByNetid[state.interactObject]
+        assert('pushPullable' in entity && entity.pushPullable instanceof sc.PushPullable)
+        this.interactObject = entity.pushPullable
+    } else this.interactObject = null
+
+    let updateStats = false
+    // prettier-ignore
+    {
+        if (state.head !== undefined) { updateStats = true; this.model.equip.head = state.head }
+        if (state.leftArm !== undefined) { updateStats = true; this.model.equip.leftArm = state.leftArm }
+        if (state.rightArm !== undefined) { updateStats = true; this.model.equip.rightArm = state.rightArm }
+        if (state.torso !== undefined) { updateStats = true; this.model.equip.torso = state.torso }
+        if (state.feet !== undefined) { updateStats = true; this.model.equip.feet = state.feet }
+    }
+
+    if (state.level !== undefined) {
+        this.model.level = state.level
+        /* I don't want to even start thinking about making this unique for every player */
+        sc.inventory.updateScaledEquipment(state.level)
+        notifyMapAndPlayerInsts(this.model, sc.PLAYER_MSG.LEVEL_CHANGE, null)
+    }
+    if (state.items) StateMemory.applyChangeRecord(this.model.items, state.items)
+    if (state.skillPoints) StateMemory.applyChangeRecord(this.model.skillPoints, state.skillPoints)
+    if (state.skills) {
+        setSkills.call(this, state.skills)
+        updateStats = true
+    }
+
+    if (updateStats) {
+        this.model.updateStats()
+    }
+
+    if (state.charge !== undefined) {
+        if (state.charge == 0) {
+            this.charging.time = 1
+            this.clearCharge()
+        } else {
+            this.showChargeEffect(state.charge)
+        }
+    }
+    if (state.element !== undefined) {
+        this.model.currentElementMode = state.element
+    }
+}
+
+prestart(() => {
+    ig.ENTITY.Player.inject({ getState, setState })
+})
