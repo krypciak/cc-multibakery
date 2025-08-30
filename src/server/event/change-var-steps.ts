e26e8d0cc641278097bd5c0f225e7a5cd5f90547
krypek <krypek@tuta.io>
2025-08-30 11:04:34 +0200

    Propagate setBy entity on CHANGE_VAR_* steps run by the client

---
+++ src/server/event/change-var-steps.ts
@@ -0,0 +1,27 @@
+import { prestart } from '../../loading-stages'
+import { PhysicsServer } from '../physics/physics-server'
+import { setNextSetBy, unsetNextSetBy } from './vars'
+
+prestart(() => {
+    if (!PHYSICS) return
+
+    function replace<T extends ig.StepBase>(this: T & { parent: T['start'] }, data: unknown, eventCall?: ig.EventCall) {
+        if (!(multi.server instanceof PhysicsServer)) return this.parent(data, eventCall)
+
+        if (ig.client) setNextSetBy(ig.game.playerEntity)
+
+        this.parent(data, eventCall)
+
+        unsetNextSetBy()
+    }
+
+    ig.EVENT_STEP.CHANGE_VAR_BOOL.inject({ start: replace })
+    ig.EVENT_STEP.CHANGE_VAR_VEC2.inject({ start: replace })
+    ig.EVENT_STEP.CHANGE_VAR_VEC3.inject({ start: replace })
+    ig.EVENT_STEP.CHANGE_VAR_NUMBER.inject({ start: replace })
+    ig.EVENT_STEP.CHANGE_VAR_STRING.inject({ start: replace })
+
+    ig.ACTION_STEP.CHANGE_VAR_BOOL.inject({ start: replace })
+    ig.ACTION_STEP.CHANGE_VAR_NUMBER.inject({ start: replace })
+    ig.ACTION_STEP.CHANGE_VAR_STRING.inject({ start: replace })
+})
