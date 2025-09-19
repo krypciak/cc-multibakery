b2734d3cfa390cdaf93f0a1b0698a86e12256902
krypek <krypek@tuta.io>
2025-09-19 19:49:04 +0200

    Use StringEnumNode in EntityStateRecordUnionNode

--- build/entity-state-record-union-node.ts
+++ build/entity-state-record-union-node.ts
@@ -2,14 +2,14 @@ import ts from 'typescript'
 import { Node } from 'ts-binarifier/src/nodes/node'
 import { green, yellow } from 'ts-binarifier/src/colors'
 import { getRecordKeyType, getRecordValueType, TypeParser } from 'ts-binarifier/src/type-parser'
-import { NumberNode } from 'ts-binarifier/src/nodes/number'
 import { findVariableDeclaration } from 'ts-binarifier/src/type-extractor'
 import { assert } from 'ts-binarifier/src/assert'
 import { StringNode } from 'ts-binarifier/src/nodes/string'
+import { StringEnumNode } from 'ts-binarifier/src/nodes/string-enum'
 
 class EntityStateRecordUnionNode extends Node {
-    private unionIdNode: NumberNode
     private netidNode = new StringNode(false, 63)
+    private entityTypeNode: StringEnumNode
 
     constructor(
         optional: boolean | undefined,
@@ -17,7 +17,7 @@ class EntityStateRecordUnionNode extends Node {
         private stringIds: string[]
     ) {
         super(optional)
-        this.unionIdNode = NumberNode.optimalForRange(false, 0, values.length)
+        this.entityTypeNode = new StringEnumNode(false, stringIds)
     }
 
     print(noColor?: boolean, indent: number = 0, ignoreOptional?: boolean) {
@@ -32,7 +32,7 @@ class EntityStateRecordUnionNode extends Node {
                         Node.indent(indent + 1) +
                         `/* id: ` +
                         yellow(`${i}`, noColor) +
-                        ` (${this.unionIdNode.print(noColor)}) typeId: ` +
+                        ` (${this.entityTypeNode.unionIdNode.print(noColor)}) typeId: ` +
                         green(`'${this.stringIds[i]}' `, noColor) +
                         `*/ ` +
                         v.print(noColor, indent + 1)
@@ -55,12 +55,14 @@ class EntityStateRecordUnionNode extends Node {
                 Node.indent(data.indent) +
                 `for (const [${netidVar}, ${valueVar}] of Object.entries(${data.varName}) as unknown as [keyof typeof ${data.varName}, any][]) {\n` +
                 Node.indent(data.indent + 1) +
-                this.netidNode.genEncode({ ...data, varName: netidVar }) +
+                this.netidNode.genEncode({ ...data, varName: netidVar, indent: data.indent + 1 }) +
                 '\n' +
                 Node.indent(data.indent + 1) +
                 `const ${idVar} = ` +
-                `[${this.stringIds.map(id => `'${id}'`).join(', ')}]` +
-                `.indexOf(${netidVar}.substring(0, 2))` +
+                this.entityTypeNode.genEncodeAccess({ ...data, varName: netidVar, indent: data.indent + 1 }) +
+                '\n' +
+                Node.indent(data.indent + 1) +
+                this.entityTypeNode.unionIdNode.genEncode({ ...data, varName: idVar, indent: data.indent + 1 }) +
                 '\n' +
                 Node.indent(data.indent + 1) +
                 `switch (${idVar}) { \n` +
@@ -97,8 +99,7 @@ class EntityStateRecordUnionNode extends Node {
                 '\n' +
                 Node.indent(data.indent + 1) +
                 `const ${idVar} = ` +
-                `[${this.stringIds.map(id => `'${id}'`).join(', ')}]` +
-                `.indexOf(${netidVar}.substring(0, 2))` +
+                this.entityTypeNode.unionIdNode.genDecode(data) +
                 '\n' +
                 Node.indent(data.indent + 1) +
                 `let ${valueVar}: any\n` +
