cc1eba5750557b16e2d2d6aafa5209cf9f0637f4
krypek <krypek@tuta.io>
2026-04-18 18:04:51 +0200

    Alias NetConnection to SocketNetConnection

--- src/net/connection.ts
+++ src/net/connection.ts
@@ -1,23 +1,4 @@
-import type { Client } from '../client/client'
+import type { SocketNetConnection, SocketNetManagerPhysicsServer } from './socket'
 
-export interface NetConnection {
-    clients: Client[]
-    bytesSent: bigint
-    bytesReceived: bigint
-    onReceive?: (data: unknown) => void
-    onDisconnect?: () => void
-
-    join(client: Client): void
-    leave(client: Client): void
-    isConnected(): boolean
-    send(type: string, data: unknown): void
-    close(): void
-    getConnectionInfo(): string
-}
-export interface NetManagerPhysicsServer {
-    connections: NetConnection[]
-
-    start(): Promise<void>
-    stop(): void
-    destroy(): void
-}
+export type NetConnection = SocketNetConnection
+export type NetManagerPhysicsServer = SocketNetManagerPhysicsServer
