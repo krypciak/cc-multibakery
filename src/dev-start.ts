2f12d5bcc92140619b0f635ec063b73f0514b84b
krypek <krypek@tuta.io>
2026-05-31 21:06:58 +0200

    Update src/dev-start.ts

--- src/dev-start.ts
+++ src/dev-start.ts
@@ -2,11 +2,13 @@ import { PhysicsServer, type PhysicsServerSettings } from './server/physics/phys
 import { DEFAULT_HTTP_PORT } from './client/menu/default-server-list'
 import { addTitleScreenButton } from './misc/title-screen-button'
 import { poststart, prestart } from './loading-stages'
-import { instanceinatorCopyInstanceConfig } from './server/server'
 import { Opts } from './options'
+import { assert } from './misc/assert'
+import { assertPhysics } from './server/physics/is-physics-server'
 
 async function startDevServer() {
     if (!PHYSICS) return
+    if (!DEV) return
 
     PROFILE && console.time('startDevServer')
 
@@ -14,7 +16,7 @@ async function startDevServer() {
         tps: 60,
         useAnimationFrameLoop: true,
         displayServerInstance: false,
-        displayMaps: true,
+        displayMaps: false,
         forceMapsActive: false,
         displayInactiveMaps: false,
         displayClientInstances: true,
@@ -24,43 +26,52 @@ async function startDevServer() {
         mapSwitchDelay: Opts.serverMapSwitchDelay,
         godmode: true,
         disablePlayerIdlePose: true,
-        // save: {
-        //     manualSaving: true,
-        //     automaticlySave: true,
-        //     loadFromSlot: 0,
-        // },
-        netInfo: {
-            connection: {
-                httpPort: DEFAULT_HTTP_PORT,
-                httpRoot: '/home/krypek/home/Programming/crosscode/instances/cc-ccloader3/crosscode-web/dist',
-                https: {
-                    cert: '/home/krypek/home/Programming/crosscode/instances/cc-ccloader3/crosscode-web/cert/localhost+1.pem',
-                    key: '/home/krypek/home/Programming/crosscode/instances/cc-ccloader3/crosscode-web/cert/localhost+1-key.pem',
-                },
-                // ccbundler: {
-                //     modProxy: true,
-                //     liveModUpdates: true,
-                // },
-                pingTimeout: 10000e3,
-                type: 'socket',
-            },
-            details: {
-                title: 'dev',
-                description: 'dev server',
-                iconPath: './assets/mods/cc-multibakery/icon/icon.png',
-                // forceJsonCommunication: true,
-            },
-        },
+        save: true
+            ? undefined
+            : {
+                  manualSaving: true,
+                  automaticlySave: true,
+                  loadFromSlot: 0,
+              },
+        netInfo: false
+            ? undefined
+            : {
+                  connection: {
+                      httpPort: DEFAULT_HTTP_PORT,
+                      https: {
+                          cert: '/home/krypek/Programming/crosscode/instances/cc-ccloader3/crosscode-web/cert/localhost+1.pem',
+                          key: '/home/krypek/Programming/crosscode/instances/cc-ccloader3/crosscode-web/cert/localhost+1-key.pem',
+                      },
+                      crosscodeWeb: true
+                          ? undefined
+                          : {
+                                httpRoot:
+                                    '/home/krypek/Programming/crosscode/instances/cc-ccloader3/crosscode-web/dist',
+                                modProxy: true,
+                                liveModUpdates: true,
+                            },
+                      pingTimeout: 10000e3,
+                      type: 'socket',
+                  },
+                  details: {
+                      title: 'dev',
+                      description: 'dev server',
+                      iconPath: './assets/mods/cc-multibakery/icon/icon.png',
+                      // forceJsonCommunication: true,
+                  },
+                  // discovery: true,
+              },
         defaultMap: {
-            // map: 'multibakery/dev',
+            map: 'multibakery/dev',
             // map: 'multibakery/mba-pvp',
+            // map: 'tree-dng/f4/boss',
             // map: 'multibakery/mba-lobby',
             // map: 'multibakery/mba-testing',
             // map: 'multibakery/mba-outdoors',
             // map: 'multibakery/mba-south',
             // map: 'bergen/hideout-lobby',
             // map: 'xpc/bonus/training-1v1',
-            map: 'rhombus-dng/room-1',
+            // map: 'rhombus-dng/room-1',
             // map: 'rhombus-dng/room-1-6',
             // map: 'rhombus-dng/boss',
             // map: 'rhombus-sqr/dng-end',
@@ -77,6 +88,7 @@ async function startDevServer() {
             // map: 'rhombus-dng/room-3-2-post',
             // map: 'rhombus-sqr/central-inner',
             // map: 'rookie-harbor/south',
+            // map: 'forest/path-10',
             // map: 'xpc/bonus/art-testing',
             // map: 'final-dng/b4/credits',
             // map: 'cursed/room-1',
@@ -101,31 +113,30 @@ async function startDevServer() {
     PROFILE && console.timeEnd('server start')
 
     if (window.crossnode?.options.test || !process.execPath.includes('server')) return
-    PROFILE && console.time('client creation')
-    server.setMasterClient(
-        await server.forceCreateClient({
-            username: `lea_${1}`,
-            inputType: 'clone',
-            remote: false,
-        })
-    )
-    PROFILE && console.timeEnd('client creation')
-    PROFILE && console.timeEnd('startDevServer')
 
-    return
+    PROFILE && console.time('client creation')
+    await createClients(1)
+    PROFILE && console.timeEnd('client creation')
+
+    if (false as boolean) splitClientsIntoGroups(5)
+
+    PROFILE && console.timeEnd('startDevServer')
+}
+
+async function createClients(count: number) {
+    if (count == 0) return
+    const { client } = await multi.server.createAndJoinClient({ username: `lea_${1}` }, { awaitClientJoin: true })
+    assert(client)
+    multi.server.setMasterClient(client)
+
     let promises = []
-    for (let i = 2; i <= 2; i++) {
-        promises.push(
-            server.forceCreateClient({
-                username: `lea_${i}`,
-                // noShowInstance: true || i != 2,
-                inputType: 'clone',
-                remote: false,
-            })
-        )
+    for (let i = 2; i <= count; i++) {
+        promises.push(multi.server.createAndJoinClient({ username: `lea_${i}` }, { awaitClientJoin: true }))
     }
     await Promise.all(promises)
+}
 
+function splitClientsIntoGroups(groupSize: number) {
     function splitIntoRandomGroups<T>(arr: T[], n: number): T[][] {
         if (n <= 0) throw new Error('n must be > 0')
         if (n > arr.length) throw new Error('n cannot exceed array length')
@@ -156,7 +167,7 @@ async function startDevServer() {
     }
 
     const clients = [...multi.server.clients.values()]
-    const groups = splitIntoRandomGroups(clients, 5)
+    const groups = splitIntoRandomGroups(clients, groupSize)
     for (const group of groups) {
         const owner = group[0]
         for (let i = 1; i < group.length; i++) {
@@ -174,7 +185,7 @@ function isInClientDir() {
 }
 
 prestart(() => {
-    if (!PHYSICS || !isInServerDir()) return
+    if (!PHYSICS || !isInServerDir() || !DEV) return
 
     addTitleScreenButton({
         text: 'Start dev server',
@@ -183,7 +194,7 @@ prestart(() => {
 })
 
 poststart(async () => {
-    if (window.crossnode?.options.test) return
+    if (!DEV && window.crossnode?.options.test) return
 
     if (PHYSICS && isInServerDir()) {
         startDevServer()
