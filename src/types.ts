8bbcc553d6c063cd3b8cc629e5595e1e1ce87502
krypek <krypek@tuta.io>
2026-07-04 22:26:33 +0200

    Update project structure

--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,6 @@ import type {} from 'nax-ccuilib/src/ui/pause-screen/pause-screen-api'
 import type {} from 'nax-ccuilib/src/ui/quick-menu/quick-menu-extension'
 import type {} from 'cc-variable-charge-time/src/plugin'
 import type {} from 'cc-krypek-lib/src/plugin'
-import type {} from 'cc-jetpack-widget/src/plugin'
 import 'ts-binarifier/src/type-aliases'
 import './net/binary/binary-types'
 
