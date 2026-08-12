a037d3b294666311a53683f6e66dbd51388b3cb3
krypek <krypek@tuta.io>
2026-08-12 22:41:14 +0200

    Adapt to new ts-binarifier api

--- build/entity-state-record-union-node.ts
+++ build/entity-state-record-union-node.ts
@@ -1,6 +1,6 @@
 import ts from 'typescript'
 import { Node } from 'ts-binarifier/src/nodes/node'
-import { gray, green, yellow } from 'ts-binarifier/src/colors'
+import { gray, yellow } from 'ts-binarifier/src/colors'
 import type { TypeParser } from 'ts-binarifier/src/type-parser'
 import { getRecordKeyType } from 'ts-binarifier/src/type-parser'
 import { assert } from 'ts-binarifier/src/assert'
@@ -17,118 +17,138 @@ class EntityStateRecordUnionNode extends Node {
         super(optional)
     }
 
-    print(noColor?: boolean, indent: number = 0, ignoreOptional?: boolean) {
-        return (
+    toString(shr: SharedPrintConfig, ind: IndividualPrintConfig) {
+        return this.toStringWrapInOptionalUnion(
+            shr,
+            ind,
             'EntityStateRecordUnion<' +
-            this.netidNode.print() +
-            ', \n' +
-            this.values
-                .map(
-                    ({ node, name }, i) =>
-                        Node.indent(indent + 1) +
-                        gray(`/* `, noColor) +
-                        gray(name, noColor) +
-                        gray(` id: `, noColor) +
-                        yellow(`${i + 1}`, noColor) +
-                        gray(` */ `, noColor) +
-                        node.print(noColor, indent + 1)
-                )
-                .join(' | \n') +
-            '>' +
-            this.optionalSuffix(ignoreOptional, noColor)
+                this.netidNode.toString(shr, { indent: ind.indent + 1 }) +
+                ', \n' +
+                this.values
+                    .map(
+                        ({ node, name }, i) =>
+                            Node.indent(ind.indent + 1) +
+                            gray(`/* `, shr.noColor) +
+                            gray(name, shr.noColor) +
+                            gray(` id: `, shr.noColor) +
+                            yellow(`${i + 1}`, shr.noColor) +
+                            gray(` */ `, shr.noColor) +
+                            node.toString(shr, { indent: ind.indent + 1 })
+                    )
+                    .join(' | \n') +
+                '>'
         )
+    }
+    private sanitizeEntityNameForFunctionName(name: string) {
+        return name.replace(/\./g, '_')
     }
 
     genEncode(data: GenEncodeData): string {
-        const netidVar = `k${data.varCounter.v++}`
-        const valueVar = `v${data.varCounter.v++}`
-        const idVar = `id${data.varCounter.v++}`
-        const entriesVar = `entries${data.varCounter.v++}`
-        data.imports.push(`import { getEntityTypeId } from '../../misc/entity-netid'`)
+        const netidVar = `netid`
+        const valueVar = `data`
+        const idVar = `typeId`
+        const entriesVar = `entries`
+        addImport(data.imports, '../../misc/entity-netid', 'getEntityTypeId')
 
-        return this.genEncodeWrapOptional(
-            data,
-            data =>
-                `const ${entriesVar} = Object.entries(${data.varName}) as unknown as [keyof typeof ${data.varName}, any][]\n` +
-                Node.indent(data.indent) +
-                this.recordSizeNode.genEncode({ ...data, varName: `${entriesVar}.length` }) +
+        const functions: FunctionConfig[] = this.values.map(({ node, name }) =>
+            getOrDefineFunction(data, {
+                name: 'encodeEntity_' + this.sanitizeEntityNameForFunctionName(name),
+                arguments: ['encoder: Encoder', `data: EntityStates['${name}']`],
+                body: node.genEncode({ ...data, varName: 'data', indent: 0 }),
+            })
+        )
+
+        addImport(data.imports, '../../state/entity', 'EntityStateRecord', true)
+
+        const statesVar = 'states'
+        const mainFunction = getOrDefineFunction(data, {
+            name: 'encodeEntityStates',
+            arguments: ['encoder: Encoder', `${statesVar}: EntityStateRecord`],
+            body:
+                `const ${entriesVar} = Object.entries(${statesVar}) as unknown as [keyof typeof ${statesVar}, any][]\n` +
+                this.recordSizeNode.genEncode({ ...data, indent: 0, varName: `${entriesVar}.length` }) +
                 '\n' +
-                Node.indent(data.indent) +
                 `for (const [${netidVar}, ${valueVar}] of ${entriesVar}) {\n` +
-                Node.indent(data.indent + 1) +
-                this.netidNode.genEncode({ ...data, varName: netidVar, indent: data.indent + 1 }) +
+                Node.indent(1) +
+                this.netidNode.genEncode({ ...data, varName: netidVar, indent: 1 }) +
                 '\n' +
-                Node.indent(data.indent + 1) +
+                Node.indent(1) +
                 `const ${idVar} = getEntityTypeId(${netidVar})` +
                 '\n' +
-                Node.indent(data.indent + 1) +
+                Node.indent(1) +
                 `switch (${idVar}) { \n` +
                 this.values
                     .map(
-                        ({ node, name }, i) =>
-                            Node.indent(data.indent + 2) +
-                            `case ${i + 1}: { // ${name}\n` +
-                            Node.indent(data.indent + 3) +
-                            node.genEncode({ ...data, varName: valueVar, indent: data.indent + 3 }) +
-                            '\n' +
-                            Node.indent(data.indent + 3) +
-                            `break\n` +
-                            Node.indent(data.indent + 2) +
+                        ({}, i) =>
+                            Node.indent(2) +
+                            `case ${i + 1}: { ` +
+                            `this.${functions[i].name}(encoder, ${valueVar}); ` +
+                            `break; ` +
                             `}\n`
                     )
                     .join('') +
-                Node.indent(data.indent + 1) +
+                Node.indent(1) +
                 `}\n` +
-                Node.indent(data.indent) +
-                `}`
-        )
+                `}`,
+        })
+
+        return this.genEncodeWrapOptional(data, data => `this.${mainFunction.name}(encoder, ${data.varName})`)
     }
 
     genDecode(data: GenDecodeData): string {
-        const netidVar = `netid${data.varCounter.v++}`
-        const valueVar = `v${data.varCounter.v++}`
-        const idVar = `id${data.varCounter.v++}`
-        return this.genDecodeWrapOptional(
-            `Object.fromEntries(new Array(` +
-                this.recordSizeNode.genDecode({ ...data }) +
-                `).fill(null).map(_ => {\n` +
-                Node.indent(data.indent + 1) +
-                `const ${netidVar} = ` +
-                this.netidNode.genDecode(data) +
-                '\n' +
-                Node.indent(data.indent + 1) +
-                `const ${idVar} = getEntityTypeId(${netidVar})` +
-                '\n' +
-                Node.indent(data.indent + 1) +
-                `let ${valueVar}: any\n` +
-                Node.indent(data.indent + 1) +
-                `switch (${idVar}) { \n` +
-                this.values
-                    .map(
-                        ({ node, name }, i) =>
-                            Node.indent(data.indent + 2) +
-                            `case ${i + 1}: { // ${name}\n` +
-                            Node.indent(data.indent + 3) +
-                            `${valueVar} = ` +
-                            node.genDecode({ ...data, indent: data.indent + 3 }) +
-                            '\n' +
-                            Node.indent(data.indent + 3) +
-                            `break\n` +
-                            Node.indent(data.indent + 2) +
-                            `}\n`
-                    )
-                    .join('') +
-                Node.indent(data.indent + 1) +
-                `}\n` +
-                Node.indent(data.indent + 1) +
-                `return [${netidVar}, ${valueVar}]\n` +
-                Node.indent(data.indent) +
-                `}))`
+        const functions: FunctionConfig[] = this.values.map(({ node, name }) =>
+            getOrDefineFunction(data, {
+                name: 'decodeEntity_' + this.sanitizeEntityNameForFunctionName(name),
+                arguments: ['decoder: Decoder'],
+                returnType: `EntityStates['${name}']`,
+                body: 'return ' + node.genDecode({ ...data, indent: 0 }),
+            })
         )
+
+        addImport(data.imports, '../../misc/entity-netid', 'EntityNetid', true)
+
+        const subFunction = getOrDefineFunction(data, {
+            name: 'decodeEntityState',
+            arguments: ['decoder: Decoder', 'netid: EntityNetid'],
+            body:
+                `const typeId = getEntityTypeId(netid)\n` +
+                `switch (typeId) {\n` +
+                this.values
+                    .map(({}, i) => Node.indent(1) + `case ${i + 1}: ` + `return this.${functions[i].name}(decoder)\n`)
+                    .join('') +
+                `}\n` +
+                `return {}`,
+        })
+
+        const mainFunction = getOrDefineFunction(data, {
+            name: 'decodeEntityStates',
+            arguments: ['decoder: Decoder'],
+            body:
+                `const len = ${this.recordSizeNode.genDecode({ ...data })}\n` +
+                `const record: EntityStateRecord = {}\n` +
+                `for (let i = 0; i < len; i++) {\n` +
+                Node.indent(1) +
+                `const netid = ${this.netidNode.genDecode(data)}\n` +
+                Node.indent(1) +
+                `record[netid] = this.${subFunction.name}(decoder, netid)\n` +
+                `}\n` +
+                `return record`,
+        })
+
+        return this.genDecodeWrapOptional(`this.${mainFunction.name}(decoder)`)
     }
 }
 
 import * as fs from 'fs'
+import type {
+    FunctionConfig,
+    GenDecodeData,
+    GenEncodeData,
+    IndividualPrintConfig,
+    SharedPrintConfig,
+} from 'ts-binarifier/src/types'
+import { addImport } from 'ts-binarifier/src/code-gen-imports'
+import { getOrDefineFunction } from 'ts-binarifier/src/code-gen-functions'
 const entityImportOrder = (await fs.promises.readFile('src/state/entity-all.ts', 'utf8'))
     .split('\n')
     .filter(line => line.startsWith("import './entity/"))
