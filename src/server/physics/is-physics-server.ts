483d1dc49ba70c543d54d981d41dd8969e4607c5
krypek <krypek@tuta.io>
2026-09-02 20:48:59 +0200

    Remove src/server/physics/is-physics-server.ts

--- src/server/physics/is-physics-server.ts
+++
@@ -1,9 +0,0 @@
-import { assert } from '../../misc/assert'
-import type { PhysicsServer } from './physics-server'
-
-export function isPhysics(server: typeof multi.server): server is PhysicsServer {
-    return server && server.physics
-}
-export function assertPhysics(server: typeof multi.server): asserts server is PhysicsServer {
-    assert(server?.physics)
-}
